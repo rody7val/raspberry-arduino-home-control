3d05f741afeb3042bb59c46abe8664061ff53a41
rody7val <rod7val@gmail.com>
2016-10-04 03:01:39 -0300

    added routes api and jade templates, led and servo

--- api.js
+++ api.js
@@ -5,31 +5,44 @@ module.exports = function (app, express) {
 
   var api = express.Router();
 
-  // Home page
-  api.get('/', function(req, res, next){
-    res.render( 'index');
+  board.on("ready", function() {
+
+    // Home page
+    api.get('/', function(req, res, next){
+      res.render( 'index');
+    });
+
+    // led
+    api.get('/led', function(req, res, next){
+      res.render( 'led');
+        var led = new five.Led(13);
+        api.get('/start-led', function(req, res){
+          led.stop()
+          led.on();
+        });
+        api.get('/stop-led', function(req, res){
+          led.stop()
+          led.off();
+        })
+        api.get('/blink-led', function(req, res){
+          led.stop()
+          led.blink(500);
+        })   
+    });
+
+    // servo
+    api.get('/servo', function(req, res, next){
+      res.render('servo');
+      var servo = new five.Servo(9);
+      api.get('/sweep-servo', function(req, res){
+        servo.sweep();
+      });
+      api.get('/stop-servo', function(req, res){
+        servo.stop();
+      });
+    });
+
   });
-
-	board.on("ready", function() {
-
-	  var led = new five.Led(13);
-
-  	api.get('/start', function(req, res){
-  		led.stop()
-  		led.on();
-  	});
-
-  	api.get('/stop', function(req, res){
-  		led.stop()
-			led.off();
-  	})
-
-		api.get('/blink', function(req, res){
-			led.stop()
-			led.blink(500);
-  	})
-
-	});
 
 
   return api;
