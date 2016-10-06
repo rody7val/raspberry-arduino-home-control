73c0e4e9f97472f17c6dab897104c1b10a7e410c
rody7val <rod7val@gmail.com>
2016-10-05 23:58:28 -0300

    fixed app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require('path');
 var app = express();
-var config = require('../config');
+var config = require('./config');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
